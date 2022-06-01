0cf14802a5e974f940b6bccbbc9b3185e4a688df
harry <dammykuye@gmail.com>
2022-06-01 16:56:09 +0100

    production setup

--- blogapi/config.py
+++ blogapi/config.py
@@ -8,3 +8,11 @@ class Test_Config:
     SERVER_NAME = 'localhost:5000'
     GOOGLE_CLIENT_ID = os.getenv('CLIENT_ID')
     GOOGLE_CLIENT_SECRET = os.getenv('CLIENT_SECRET')
+
+class ProductionConfig:
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    PAGINATION_SIZE_PER_PAGE = 10
+    GOOGLE_CLIENT_ID = os.environ.get('CLIENT_ID')
+    GOOGLE_CLIENT_SECRET = os.environ.get('CLIENT_SECRET')
